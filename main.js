7bedd38c1b3b95de7893a101b90bf0cfe6d6beab
Adnan Shahid <adnansh2804@gmail.com>
2023-02-12 18:08:13 +0530

    homepage update

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".tags", {
+    strings: ["Competitive Programmer", "Developer","Web3 Enthusiast" , "Designer", "Badminton Player", ""],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
