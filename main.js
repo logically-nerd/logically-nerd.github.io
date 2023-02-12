a0c775ea7ab9c6f0f1d8a68a75c36dd1e24fb11f
Adnan Shahid <adnansh2804@gmail.com>
2023-02-13 00:53:04 +0530

    responsive home

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".tags", {
-    strings: ["Competitive Programmer", "Developer","Web3 Enthusiast" , "Designer", "Badminton Player", ""],
+    strings: ["Competitive Programmer", "Developer","Web3 Enthusiast" , "Designer", "Badminton Player"],
     typeSpeed: 70,
     backSpeed: 50,
     loop: true
