bc8874af796a9c77a423f7e06fafccf342dad5a0
Adnan Shahid <adnansh2804@gmail.com>
2023-02-13 00:22:26 +0530

    responsive nav

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ var typed = new Typed(".tags", {
     backSpeed: 50,
     loop: true
 });
+
+var x = document.querySelector("ul");
+var y = document.querySelector(".hamburger");
+function myFunction() {
+    x.classList.toggle("ul_block");
+    // x.style.top = "0vh";
+    y.classList.toggle('opened');
+    y.setAttribute('aria-expanded', y.classList.contains('opened'));
+}
