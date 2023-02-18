f065cd86df0cb07227683e89511859e29595ab31
Adnan Shahid <adnansh2804@gmail.com>
2023-02-18 11:08:28 +0530

    skills complete

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+//type animation
 var typed = new Typed(".tags", {
-    strings: ["Competitive Programmer", "Developer","Web3 Enthusiast" , "Designer", "Badminton Player"],
+    strings: ["Competitive Programmer", "Developer", "Web3 Enthusiast", "Designer", "Badminton Player"],
     typeSpeed: 70,
     backSpeed: 50,
     loop: true
 });
 
+//hamburger menu
 var x = document.querySelector("ul");
 var y = document.querySelector(".hamburger");
 function myFunction() {
@@ -13,3 +15,12 @@ function myFunction() {
     y.classList.toggle('opened');
     y.setAttribute('aria-expanded', y.classList.contains('opened'));
 }
+
+//multiple titles
+
+let titleText = ["Adnan Shahid", "Welcome to my website", "logically-nerd", "Welcome to my website"];
+let counter = 0;
+setInterval(function () {
+    document.title = titleText[counter % titleText.length];
+    counter++;
+}, 2000)
