581078944b46b275584d544568a9f24c1030e55f
logically-nerd <adnansh2804@gmail.com>
2024-08-19 00:54:15 +0530

    bug fix

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -72,7 +72,7 @@ export const FloatingNav = ({
                         <span className="hidden sm:block text-sm">{navItem.name}</span>
                     </Link>
                 ))}
-                <a href="https://docs.google.com/document/d/1kUiMz7q9dr9Yn2NGNp038x9MX7X5iIasFl5-Cs-9LB4/edit?usp=sharing">
+                <a href="https://docs.google.com/document/d/1kUiMz7q9dr9Yn2NGNp038x9MX7X5iIasFl5-Cs-9LB4/edit?usp=sharing" target="_blank">
                     <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
                         <span>Resume</span>
                         <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
